31e4d3435e593e2dbf0a8a54e08a88eb830e995e
Eric M. F. Freitas <142802977+Eric6ix@users.noreply.github.com>
2024-09-26 14:21:22 -0300

    banco reformulado
    eric

---
+++ SQLQuery_banco_Main.sql
@@ -0,0 +1,285 @@
+USE [master]
+GO
+/****** Object:  Database [AcademiaDEV]    Script Date: 26/09/2024 14:04:15 ******/
+CREATE DATABASE [AcademiaDEV]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'AcademiaDEV', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS2022\MSSQL\DATA\AcademiaDEV.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'AcademiaDEV_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS2022\MSSQL\DATA\AcademiaDEV_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [AcademiaDEV] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [AcademiaDEV].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [AcademiaDEV] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET AUTO_CLOSE ON 
+GO
+ALTER DATABASE [AcademiaDEV] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [AcademiaDEV] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [AcademiaDEV] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [AcademiaDEV] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [AcademiaDEV] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [AcademiaDEV] SET  MULTI_USER 
+GO
+ALTER DATABASE [AcademiaDEV] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [AcademiaDEV] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [AcademiaDEV] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [AcademiaDEV] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [AcademiaDEV] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [AcademiaDEV] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [AcademiaDEV] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [AcademiaDEV] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [AcademiaDEV]
+GO
+/****** Object:  Table [dbo].[Alunos]    Script Date: 26/09/2024 14:04:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Alunos](
+	[aluno_id] [int] IDENTITY(1,1) NOT NULL,
+	[nome] [varchar](100) NOT NULL,
+	[CPF] [varchar](14) NOT NULL,
+	[email] [varchar](100) NULL,
+	[senha] [varchar](50) NULL,
+	[data_nascimento] [date] NOT NULL,
+	[endereco] [varchar](255) NULL,
+	[telefone] [varchar](15) NULL,
+	[informacoes_medicas] [text] NULL,
+	[data_matricula] [date] NOT NULL,
+	[peso] [float] NOT NULL,
+	[altura] [float] NOT NULL,
+	[credencial] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[aluno_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[CPF] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Funcionarios]    Script Date: 26/09/2024 14:04:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Funcionarios](
+	[funcionario_id] [int] IDENTITY(1,1) NOT NULL,
+	[nome] [varchar](100) NOT NULL,
+	[telefone] [varchar](15) NULL,
+	[email] [varchar](100) NULL,
+	[senha] [varchar](50) NULL,
+	[CPF] [varchar](14) NOT NULL,
+	[salario] [money] NULL,
+	[horario_trabalho] [varchar](50) NULL,
+	[credencial] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[funcionario_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[CPF] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  View [dbo].[Vw_UsuarioLogin_Aluno_Funcionario]    Script Date: 26/09/2024 14:04:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [dbo].[Vw_UsuarioLogin_Aluno_Funcionario] as 
+select 
+a.nome as Nome_Alunos,
+a.credencial as Cred_Alunos,
+a.CPF as CPF_Alunos,
+f.nome as Nome_Funcionarios,
+f.credencial as Cred_Funcionarios,
+f.CPF as CPF_Funcionarios
+from Alunos a, Funcionarios f;
+
+GO
+/****** Object:  Table [dbo].[Aulas]    Script Date: 26/09/2024 14:04:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Aulas](
+	[aula_id] [int] IDENTITY(1,1) NOT NULL,
+	[fk_instrutor_id] [int] NOT NULL,
+	[nome_aula] [varchar](50) NOT NULL,
+	[descricao] [text] NULL,
+	[horario_inicio] [time](7) NOT NULL,
+	[horario_fim] [time](7) NOT NULL,
+	[data_aula] [date] NOT NULL,
+	[treinos] [varchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[aula_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Pagamentos]    Script Date: 26/09/2024 14:04:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Pagamentos](
+	[pagamento_id] [int] IDENTITY(1,1) NOT NULL,
+	[fk_aluno_id] [int] NOT NULL,
+	[fk_plano_id] [int] NOT NULL,
+	[data_pagamento] [date] NOT NULL,
+	[status_pagamento] [varchar](10) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[pagamento_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Planos]    Script Date: 26/09/2024 14:04:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Planos](
+	[plano_id] [int] IDENTITY(1,1) NOT NULL,
+	[nome_plano] [varchar](50) NOT NULL,
+	[duracao_meses] [date] NOT NULL,
+	[preco] [money] NOT NULL,
+	[descricao] [text] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[plano_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Produtos]    Script Date: 26/09/2024 14:04:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Produtos](
+	[produto_id] [int] IDENTITY(1,1) NOT NULL,
+	[nome] [varchar](100) NOT NULL,
+	[descricao] [text] NULL,
+	[preco] [money] NOT NULL,
+	[data_fab] [date] NULL,
+	[data_val] [date] NULL,
+	[fk_aluno_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[produto_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Relatorios]    Script Date: 26/09/2024 14:04:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Relatorios](
+	[relatorio_id] [int] IDENTITY(1,1) NOT NULL,
+	[descricao] [text] NULL,
+	[data_geracao] [date] NOT NULL,
+	[fk_gerado_por] [int] NOT NULL,
+	[tipo_relatorio] [varchar](20) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[relatorio_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Reservas]    Script Date: 26/09/2024 14:04:15 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Reservas](
+	[reserva_id] [int] IDENTITY(1,1) NOT NULL,
+	[fk_aluno_id] [int] NOT NULL,
+	[fk_aula_id] [int] NOT NULL,
+	[data_reserva] [date] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[reserva_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Aulas]  WITH CHECK ADD FOREIGN KEY([fk_instrutor_id])
+REFERENCES [dbo].[Funcionarios] ([funcionario_id])
+GO
+ALTER TABLE [dbo].[Pagamentos]  WITH CHECK ADD FOREIGN KEY([fk_aluno_id])
+REFERENCES [dbo].[Alunos] ([aluno_id])
+GO
+ALTER TABLE [dbo].[Pagamentos]  WITH CHECK ADD FOREIGN KEY([fk_plano_id])
+REFERENCES [dbo].[Planos] ([plano_id])
+GO
+ALTER TABLE [dbo].[Produtos]  WITH CHECK ADD FOREIGN KEY([fk_aluno_id])
+REFERENCES [dbo].[Alunos] ([aluno_id])
+GO
+ALTER TABLE [dbo].[Relatorios]  WITH CHECK ADD FOREIGN KEY([fk_gerado_por])
+REFERENCES [dbo].[Funcionarios] ([funcionario_id])
+GO
+ALTER TABLE [dbo].[Reservas]  WITH CHECK ADD FOREIGN KEY([fk_aluno_id])
+REFERENCES [dbo].[Alunos] ([aluno_id])
+GO
+ALTER TABLE [dbo].[Reservas]  WITH CHECK ADD FOREIGN KEY([fk_aula_id])
+REFERENCES [dbo].[Aulas] ([aula_id])
+GO
+USE [master]
+GO
+ALTER DATABASE [AcademiaDEV] SET  READ_WRITE 
+GO
